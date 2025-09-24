f33a2c8b563d687b2de12ff8035befc8a7d93bdd
Mark Josephs <95541571+mjosephs04@users.noreply.github.com>
2025-09-24 16:12:43 -0500

    Update init.sql

--- init.sql
+++ init.sql
@@ -7,3 +7,4 @@ CREATE DATABASE IF NOT EXISTS todd CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode
 -- Grant permissions to todd_user
 GRANT ALL PRIVILEGES ON todd.* TO 'todd_user'@'%';
 FLUSH PRIVILEGES;
+
